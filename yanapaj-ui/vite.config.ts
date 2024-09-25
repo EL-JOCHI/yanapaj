0aa7caceb6980acdf1ff523a7ae198a4e177ef57
Miguel Alejandro Hurtado Orellana <miguelalejandrohurtadoorellana@gmail.com>
2024-09-24 21:16:32 -0400

    YAN-8 📦 Adding CRUD for Tasks - ✨ [2024-09-24] Replace react to vite. - ✨ [2024-09-24] Set up theming. - ✨ [2024-09-24] Update Docker set up.

---
+++ yanapaj-ui/vite.config.ts
@@ -0,0 +1,24 @@
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+})
