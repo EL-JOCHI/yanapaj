a9ec39307d551b22d1107ca496d3037cd11a5930
Miguel Alejandro Hurtado Orellana <miguelalejandrohurtadoorellana@gmail.com>
2024-09-25 08:57:22 -0400

    YAN-8 📦 Adding CRUD for Tasks - 🔖 [2024-09-25] Added generation of JWT key using a bash script.

--- yanapaj-service/generate_jwt_secret.sh
+++ yanapaj-service/generate_jwt_secret.sh
@@ -5,5 +5,8 @@ JWT_SECRET=$(openssl rand -base64 32)
 
 # Update the application.properties file with the generated secret
 sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" src/main/resources/application.properties
+echo "✅  JWT Secret updated in src/main/resources/application.properties"
 
-echo "✅ JWT Secret updated in application.properties"
+# Update the application-test.properties file with the generated secret
+sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" src/test/resources/application-test.properties
+echo "🧪 JWT Secret updated in src/test/resources/application-test.properties"
