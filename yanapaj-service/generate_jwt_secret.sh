597a2cf10f9169d6b4329b2cc1c50e30b2271312
Miguel Alejandro Hurtado Orellana <miguelalejandrohurtadoorellana@gmail.com>
2024-10-02 21:24:24 -0400

    🐛 [2024-10-02] Resolve issue where sed command is unable to find the properties file

--- yanapaj-service/generate_jwt_secret.sh
+++ yanapaj-service/generate_jwt_secret.sh
@@ -3,10 +3,14 @@
 # Generate a strong, URL-safe JWT secret key
 JWT_SECRET=$(openssl rand -base64 32)
 
+echo "🚀 JWT Secret: $JWT_SECRET"
+
 # Update the application.properties file with the generated secret
-sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" src/main/resources/application.properties
+# sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" src/main/resources/application.properties
+find . -name "application.properties" -exec sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" {} \;
 echo "✅  JWT Secret updated in src/main/resources/application.properties"
 
 # Update the application-test.properties file with the generated secret
-sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" src/test/resources/application-test.properties
+# sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" src/test/resources/application-test.properties
+find . -name "application-test.properties" -exec sed -i '' "s|jwt.secret=.*|jwt.secret=$JWT_SECRET|" {} \;
 echo "🧪 JWT Secret updated in src/test/resources/application-test.properties"
